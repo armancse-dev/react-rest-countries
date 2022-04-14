649f8fd003f67b011beeec3b9e0340ffef508ea0
armancse-dev <armanhossaincse7@gmail.com>
2022-04-14 23:31:17 +0600

    Display countries in a simple way, Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Countries></Countries>
+      {/* <Countries></Countries> */}
+      
     </div>
   );
 }
 
-function Countries(){
-  const [countries, setCountries] = useState([]);
-  useEffect( () => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data =>setCountries(data));
-  }, []);
-  return(
-    <div>
-      <h2>Traveling around the world!</h2>
-      <h4>Countries Avaiable: {countries.length}</h4>
-    </div>
-  )
-}
+// function Countries(){
+//   const [countries, setCountries] = useState([]);
+  
+//   useEffect( () => {
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data =>setCountries(data));
+//   }, []);
+
+//   return(
+//     <div>
+//       <h2>Traveling around the world!</h2>
+//       <h4>Countries Avaiable: {countries.length}</h4>
+//       {
+//         countries.map(country => <Country name={country.name.common} capital={country.capital}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// function Country(props){
+//   return(
+//     <div>
+//       <h2>Name: {props.name} </h2>
+//       <p>Capital: {props.capital} </p>
+//     </div>
+//   )
+// }
 
 export default App;
